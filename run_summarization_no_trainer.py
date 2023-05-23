570e86bc2870fe78aefcd45854b75e49c3422f49
Zach Mueller <muellerzr@gmail.com>
2023-05-23 15:35:34 -0400

    Nits

--- run_summarization_no_trainer.py
+++ run_summarization_no_trainer.py
@@ -55,9 +55,6 @@ from transformers.utils import check_min_version, get_full_repo_name, is_offline
 from transformers.utils.versions import require_version
 
 
-# Will error if the minimal version of Transformers is not installed. Remove at your own risks.
-check_min_version("4.30.0.dev0")
-
 logger = get_logger(__name__)
 require_version("datasets>=1.8.0", "To fix: pip install -r examples/pytorch/summarization/requirements.txt")
 
